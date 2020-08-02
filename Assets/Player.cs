28329e1e378958b7f97914daee18ccdf4e913e2e
Prathamesh Mohod <68148243+TechPrathamesh@users.noreply.github.com>
2020-08-03 00:01:52 +0530

    Add files via upload

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform playerPosition;
+    public float speed;
+    public float acceleration;
+    public float speed1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        speed += acceleration;
+
+        transform.Translate(Input.GetAxis("Horizontal") * speed1 * Time.deltaTime, 0, 0);
+    }
+}
